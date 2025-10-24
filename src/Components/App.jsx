3cdb3b2407c6b353305fc6d792173f865bd246fb
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-10-24 20:02:17 +0530

    errors are fxed

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -10,7 +10,7 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
   method:'GET',
   headers:{
-    accept:'appplication/json',
+    accept:'application/json',
     Authorization : `Bearer ${API_KEY}`
   }
 }
